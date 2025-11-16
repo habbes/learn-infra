9e9df52fffa04679dbf563f84e82635c371b321f
Clément Habinshuti <haby_habbes@live.com>
2025-11-16 10:34:49 +0300

    Create static website in storage account

--- pulumi/quickstart/Program.cs
+++ pulumi/quickstart/Program.cs
@@ -32,9 +32,28 @@ return await Pulumi.Deployment.RunAsync(() =>
         return Output.CreateSecret(firstKey);
     });
 
+    // Enable static website support
+    var staticWebsite = new StorageAccountStaticWebsite("staticWebsite", new StorageAccountStaticWebsiteArgs
+    {
+        AccountName = storageAccount.Name,
+        ResourceGroupName = resourceGroup.Name,
+        IndexDocument = "index.html"
+    });
+
+    var indexHtml = new Blob("index.html", new BlobArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        AccountName = storageAccount.Name,
+        ContainerName = staticWebsite.ContainerName,
+        Source = new FileAsset("./index.html"),
+        ContentType = "text/html"
+    });
+
     // Export the primary key of the Storage Account
+    // And the endpoint of the static website
     return new Dictionary<string, object?>
     {
-        ["primaryStorageKey"] = primaryStorageKey
+        ["primaryStorageKey"] = primaryStorageKey,
+        ["staticEndpoint"] = storageAccount.PrimaryEndpoints.Apply(primaryEndpoints => primaryEndpoints.Web)
     };
 });
